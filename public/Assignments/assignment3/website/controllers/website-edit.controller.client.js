05eeec5769c56bca3c52cb0ed301ebc830891f52
Shail Shah <shail@ccs.neu.edu>
2017-02-15 18:57:21 -0500

    Fixes website update bug

--- public/Assignments/assignment3/website/controllers/website-edit.controller.client.js
+++ public/Assignments/assignment3/website/controllers/website-edit.controller.client.js
@@ -6,11 +6,13 @@
     function websiteEditController($routeParams, WebsiteService, $location) {
         var vm = this;
         vm.userId = $routeParams.uid;
-        vm.websites = WebsiteService.findAllWebsitesForUser(vm.userId);
-        vm.websiteId = $routeParams.wid;
+        vm.websiteId=$routeParams.wid;
 
-        vm.deleteWebsite=deleteWebsite;
-        vm.updateWebsite=updateWebsite;
+        console.log(vm.userId);
+
+        vm.deleteWebsite = deleteWebsite;
+        vm.updateWebsite = updateWebsite;
+
 
         function init(){
             vm.websites = WebsiteService.findAllWebsitesForUser(vm.userId);
@@ -19,16 +21,12 @@
         init();
 
         function updateWebsite(){
-            console.log("Trying to update");
             WebsiteService.updateWebsite(vm.websiteId, vm.website);
-            vm.websites = WebsiteService.findAllWebsitesForUser(vm.userId);
         };
 
         function deleteWebsite () {
-            console.log("Trying to delete");
             WebsiteService.deleteWebsite(vm.websiteId);
             $location.url("/user/"+vm.userId+"/websites");
-            vm.websites = WebsiteService.findAllWebsitesForUser(vm.userId);
         };
     }
 })();
--- public/Assignments/assignment3/website/controllers/website-new.controller.client.js
+++ public/Assignments/assignment3/website/controllers/website-new.controller.client.js
@@ -5,20 +5,17 @@
 
     function websiteNewController($routeParams, WebsiteService, $location) {
         var vm = this;
-        vm.userId = $routeParams.uid;
-        vm.websites = WebsiteService.findAllWebsitesForUser(vm.userId);
 
         vm.create = create;
 
         function init(){
+            vm.userId = $routeParams.uid;
             vm.websites = WebsiteService.findAllWebsitesForUser(vm.userId);
             vm.website = WebsiteService.findWebsiteById(vm.websiteId);
         }
         init();
 
         function create(){
-            console.log("Trying to create.");
-            console.log(vm.website);
             WebsiteService.createWebsite(vm.userId, vm.website);
             vm.websites = WebsiteService.findAllWebsitesForUser(vm.userId);
             $location.url("/user/"+vm.userId+"/websites");
